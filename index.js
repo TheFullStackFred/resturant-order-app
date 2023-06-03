89125ab8d25f71fa58097ea098f501f6156c3a72
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-06-03 21:57:26 +0200

    Render out menu

---
+++ index.js
@@ -0,0 +1,25 @@
+import { menuArray } from './data.js'
+
+function getMenuHtml() {
+  let menuHtml = ''
+
+  menuArray.forEach(function (menuItem) {
+    console.log(menuItem)
+
+    menuHtml += `
+    <div class="menu-item-container">
+    <h2 class="menu-item-emoji">${menuItem.emoji}</h2>
+    <h3 class="menu-item-name">${menuItem.name}</h3>
+    <p class="menu-item-ingredients">${menuItem.ingredients}</p>
+    <p class="menu-item-price">${menuItem.price}$</p>
+    </div>
+    `
+  })
+  return menuHtml
+}
+
+function render() {
+  document.getElementById('menu-items').innerHTML = getMenuHtml()
+}
+
+render()
